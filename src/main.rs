f72d8907da12b41dec1aec3454fc151f4badf0cd
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-09-11 21:26:41 +0900

    get file

--- src/main.rs
+++ src/main.rs
@@ -26,11 +26,41 @@ fn world() -> &'static str {
     "Hello, world!"
 }
 
+use rocket::serde::Deserialize;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+#[derive(Deserialize, Debug)]
+#[serde(crate = "rocket::serde")]
+struct User {
+    fingerprint: String,
+    location: String,
+}
+
+fn read_user_from_file<P: AsRef<Path>>(path: P) -> Result<User, Box<dyn Error>> {
+    // Open the file in read-only mode with buffer.
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    // Read the JSON contents of the file as an instance of `User`.
+    let u = serde_json::from_reader(reader)?;
+
+    // Return the `User`.
+    Ok(u)
+}
+
 // json response
 #[get("/json")]
 fn json() -> content::Json<&'static str> {
-    content::Json("{ 'hi': 'world' }")
+
+    let u = read_user_from_file("test.json").unwrap();
+    println!("{:#?}", u);
+
+    content::Json("{ 'status': 'ok' }")
 }
+
 
 // Try visiting:
 //   http://127.0.0.1:8000/hello/мир
