8fa686f358382689e4e1057646459a12c6c56259
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-09-11 21:35:22 +0900

    read_json

--- src/main.rs
+++ src/main.rs
@@ -26,36 +26,14 @@ fn world() -> &'static str {
     "Hello, world!"
 }
 
-use rocket::serde::Deserialize;
-use std::error::Error;
-use std::fs::File;
-use std::io::BufReader;
-use std::path::Path;
-
-#[derive(Deserialize, Debug)]
-#[serde(crate = "rocket::serde")]
-struct User {
-    fingerprint: String,
-    location: String,
-}
-
-fn read_user_from_file<P: AsRef<Path>>(path: P) -> Result<User, Box<dyn Error>> {
-    // Open the file in read-only mode with buffer.
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-
-    // Read the JSON contents of the file as an instance of `User`.
-    let u = serde_json::from_reader(reader)?;
-
-    // Return the `User`.
-    Ok(u)
-}
+mod read_json;
 
 // json response
 #[get("/json")]
 fn json() -> content::Json<&'static str> {
 
-    let u = read_user_from_file("test.json").unwrap();
+    // let u = read_json::read_user_from_file("data/sample.json").unwrap();
+    let u = read_json::read_user_from_file("test.json").unwrap();
     println!("{:#?}", u);
 
     content::Json("{ 'status': 'ok' }")
---
+++ src/read_json.rs
@@ -0,0 +1,24 @@
+use rocket::serde::Deserialize;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+#[derive(Deserialize, Debug)]
+#[serde(crate = "rocket::serde")]
+pub struct User {
+    fingerprint: String,
+    location: String,
+}
+
+pub fn read_user_from_file<P: AsRef<Path>>(path: P) -> Result<User, Box<dyn Error>> {
+    // Open the file in read-only mode with buffer.
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    // Read the JSON contents of the file as an instance of `User`.
+    let u = serde_json::from_reader(reader)?;
+
+    // Return the `User`.
+    Ok(u)
+}
